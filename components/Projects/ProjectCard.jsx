acdbe9421ca76be5c4eee781f4e2cd5809e2f30f
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2022-01-31 03:06:02 +0530

    refactor: major design changes in projects page inorder to get around the image not loading in prod issue.

--- components/Projects/ProjectCard.jsx
+++ components/Projects/ProjectCard.jsx
@@ -1,59 +1,84 @@
-import Image from "next/image";
+import { useState } from "react";
+
 export const ProjectCard = ({ project }) => {
-  const {
-    name,
-    description,
-    imgSrc,
-    techStack,
-    liveLink,
-    codeFrontend,
-    codeBackend,
-  } = project;
+  const [isCursorOnCard, setIsCursonOnCard] = useState(false);
+  const { name, description, techStack, liveLink, codeFrontend, codeBackend } =
+    project;
   return (
-    <div className="flex flex-col gap-6 border bg-white p-4 shadow-xl">
-      <div className="flex flex-col gap-2 sm:flex-row">
-        <section className="flex flex-col justify-start gap-4 sm:w-2/3 ml-4 ">
-          <div className="text-xl font-medium text-gray-700">{name}</div>
-          <div>{description}</div>
-          <div>
-            <span className="font-medium">Tech Stack:</span>
-            {techStack.join(", ")}
-          </div>
-        </section>
-        <section className="w-full text-center sm:w-1/3 shadow-sm">
-          <Image src={imgSrc} alt={name} width={260} height={150} />
-        </section>
-      </div>
-      <div className="flex flex-col sm:flex-row justify-between">
+    <div
+      className="flex flex-col gap-4 justify-between border rounded bg-white p-4 shadow-xl hover:ring ring-offset-2 ring-red-500/25 ring-offset-red-100"
+      onMouseEnter={() => {
+        setIsCursonOnCard(true);
+      }}
+      onMouseLeave={() => {
+        setIsCursonOnCard(false);
+      }}
+    >
+      <div>
         <a
+          className={`text-xl ${
+            isCursorOnCard
+              ? "text-red-500 font-bold"
+              : "text-gray-900 font-medium"
+          } `}
           href={liveLink}
-          className="px-4 py-2 bg-red-500 hover:bg-red-600 text-white border rounded-md w-fit"
           target="_blank"
           rel="noreferrer"
         >
-          See Live
+          {name}
         </a>
-        <div className="flex gap-1">
-          <a
-            href={codeFrontend}
-            className="px-4 py-2 bg-sky-600 hover:bg-sky-700 text-white border rounded-md w-fit text-center"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Source Code (Frontend)
-          </a>
-          {codeBackend && (
-            <a
-              href={codeBackend}
-              className="px-4 py-2 text-sky-600 border border-sky-600 hover:text-sky-700 hover:border-sky-700 rounded-md w-fit text-center"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Source Code (Backend)
-            </a>
-          )}
+        <div className="mt-4 text-base text-gray-600">{description}</div>
+        <div className="mt-2 text-base text-">
+          <span className="font-medium text-gray-700">Tech Stack: </span>
+          {techStack.join(", ")}
         </div>
       </div>
+      <ProjectCardButtons
+        liveLink={liveLink}
+        codeFrontend={codeFrontend}
+        codeBackend={codeBackend}
+      />
     </div>
+  );
+};
+
+const ProjectCardButtons = ({ liveLink, codeFrontend, codeBackend }) => {
+  return (
+    <div className="flex flex-wrap gap-1 items-center">
+      <CustomLinkButton url={liveLink} linkType="liveLink" linkText="Live" />
+      <CustomLinkButton
+        url={codeFrontend}
+        linkType="sourceCodeFrontend"
+        linkText="Frontend Code"
+      />
+      {codeBackend && (
+        <CustomLinkButton
+          url={codeBackend}
+          linkType="sourceCodeBackend"
+          linkText="Backend Code"
+        />
+      )}
+    </div>
+  );
+};
+
+const CustomLinkButton = ({ url, linkType, linkText }) => {
+  const linkStyles = {
+    liveLink:
+      "px-4 py-2 text-center border rounded-md w-fit bg-red-600 text-white hover:bg-red-700 animate-pulse",
+    sourceCodeFrontend:
+      "px-4 py-2 text-center border rounded-md w-fit bg-sky-600 text-white hover:bg-sky-700",
+    sourceCodeBackend:
+      "px-4 py-2 text-center border rounded-md w-fit bg-white text-sky-600 border-sky-600 hover:bg-sky-50",
+  };
+  return (
+    <a
+      href={url}
+      className={linkStyles[linkType]}
+      target="_blank"
+      rel="noreferrer"
+    >
+      {linkText}
+    </a>
   );
 };
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,8 +1,5 @@
 import Layout from "../../components/Layout.jsx";
 import { ProjectCard } from "../../components/Projects/ProjectCard.jsx";
-import imgQuizzoMania from "../../public/images/screenshot-quizzomania.png";
-import imgFinMart from "../../public/images/screenshot-finmart.png";
-import imgFinTube from "../../public/images/screenshot-fintube.png";
 
 const Projects = () => {
   const projectsList = [
@@ -10,7 +7,6 @@ const Projects = () => {
       name: "QuizzoMania",
       description:
         "A webapp which allows users to attempt quizzes on a variety of topics, and supports custom JWT Authentication.",
-      imgSrc: imgQuizzoMania,
       techStack: ["ReactJS", "ExpressJS", "MongoDB", "TailwindCSS"],
       liveLink: "https://quizzo-mania-shivaansh.netlify.app/",
       codeFrontend:
@@ -21,8 +17,7 @@ const Projects = () => {
       name: "FinMart",
       description:
         "One stop destination to get the books you need to improve your financial wisdom.",
-      imgSrc: imgFinMart,
-      techStack: ["React", "ExpressJS", "MongoDB", "CSS3"],
+      techStack: ["ReactJS", "ExpressJS", "MongoDB", "CSS3"],
       liveLink: "https://fin-mart-dev.netlify.app/",
       codeFrontend: "https://github.com/Shivaansh-Agarwal/fin-mart",
       codeBackend: "https://github.com/Shivaansh-Agarwal/fin-mart-backend",
@@ -31,7 +26,6 @@ const Projects = () => {
       name: "FinTube",
       description:
         "A Place to learn all there's to learn about Personal Finance, Investing from a list of high quality curated content from Youtube.",
-      imgSrc: imgFinTube,
       techStack: ["ReactJS", "CSS3"],
       liveLink: "https://fin-tube-dev.netlify.app/",
       codeFrontend: "https://github.com/Shivaansh-Agarwal/fin-tube",
@@ -41,7 +35,7 @@ const Projects = () => {
   return (
     <Layout title="Shivaansh | Projects">
       <h1 className="text-4xl font-medium">Projects</h1>
-      <div className="mt-8 flex flex-col gap-8">
+      <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 gap-4">
         {projectsList.map((project) => {
           return <ProjectCard key={project.name} project={project} />;
         })}
